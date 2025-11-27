c39e4fd0fd5c7a2be180dcf72ad46c0c0a1520e1
Luis Matheus <luisamp971@gmail.com>
2025-11-26 16:41:01 -0800

    Creates database connection

---
+++ supabase/app/db.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models.users import Base   
+
+DATABASE_URL = "postgresql://postgres.qtsoduzzgzphapjzfkld:xtgd4s8jHxLsedbL@aws-1-us-east-1.pooler.supabase.com:5432/postgres"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
