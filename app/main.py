9fcfe4d1a70dd2cf74bffb51ef5f8d25304ea40c
Luis Matheus <luisamp971@gmail.com>
2026-05-01 05:01:19 -0700

    a

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://frontend-production-134c.up.railway.app"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://frontend-production-134c.up.railway.app", "https://smartcalendar-production-1b76.up.railway.app/login"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
