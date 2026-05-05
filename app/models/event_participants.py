fb1442a660a78f3c340d6957c131a20537df08d2
Luis Matheus <123834963+1102-perdomo-luis-matheus@users.noreply.github.com>
2026-05-05 00:15:58 -0700

    Add documentation to event_participants model
    Added documentation comments for the Event_Participants model.

--- app/models/event_participants.py
+++ app/models/event_participants.py
@@ -1,3 +1,6 @@
+# Events_Participants Model
+# Defines an ORM model that maps the event_participants table in the database to a Python class.
+# Written by: Luis Matheus Perdomo
 from sqlalchemy import Column, String, Text, TIMESTAMP
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.dialects.postgresql import UUID
